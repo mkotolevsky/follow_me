53b3f4dd7220de8825cd3815d1b35aaa8f1cb899
kotolevsky maxim n <63235486+mkotolevsky@users.noreply.github.com>
2023-02-10 10:07:46 +0300

    init

---
+++ app.js
@@ -0,0 +1,30 @@
+const board = document.querySelector('#board')
+const colors = ['#2D4628', '#E83100', '#FF6933', '#FFA570', '#FAD074']
+const SQUARES_NUMBERS = 500
+
+for(let i = 0; i < SQUARES_NUMBERS; i++) {
+    const square = document.createElement('div')
+    square.classList.add('square')
+
+    square.addEventListener('mouseover', setColor)
+    square.addEventListener('mouseleave', removeColor)
+
+    board.append(square)
+}
+
+function setColor(event) {
+    const element = event.target
+    const color = getRandomColor()
+    element.style.backgroundColor = color
+    element.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`
+}
+
+function removeColor(event) {
+    const element = event.target
+    element.style.backgroundColor = '#1d1d1d'
+    element.style.boxShadow = '0 0 2px #000'
+}
+
+function getRandomColor() {
+    return colors[Math.floor(Math.random() * colors.length)]
+}
